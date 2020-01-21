36568f4fcb3be26ef2a7e56d817f573137308a70
The AI Guy <56439609+theAIGuysCode@users.noreply.github.com>
2020-01-20 20:20:05 -0500

    Added ability to run detections on webcam.

--- detect.py
+++ detect.py
@@ -94,6 +94,50 @@ def main(type, iou_threshold, confidence_threshold, input_names):
                 cap.release()
                 print('Detections have been saved successfully.')
 
+    elif type == 'webcam':
+        inputs = tf.compat.v1.placeholder(tf.float32, [1, *_MODEL_SIZE, 3])
+        detections = model(inputs, training=False)
+        saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(scope='yolo_v3_model'))
+
+        with tf.compat.v1.Session() as sess:
+            saver.restore(sess, './weights/model.ckpt')
+
+            win_name = 'Webcam detection'
+            cv2.namedWindow(win_name)
+            cap = cv2.VideoCapture(0)
+            frame_size = (cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+                          cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fourcc = cv2.VideoWriter_fourcc(*'X264')
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            out = cv2.VideoWriter('./detections/detections.mp4', fourcc, fps,
+                                  (int(frame_size[0]), int(frame_size[1])))
+
+            try:
+                while True:
+                    ret, frame = cap.read()
+                    if not ret:
+                        break
+                    resized_frame = cv2.resize(frame, dsize=_MODEL_SIZE[::-1],
+                                               interpolation=cv2.INTER_NEAREST)
+                    detection_result = sess.run(detections,
+                                                feed_dict={inputs: [resized_frame]})
+
+                    draw_frame(frame, frame_size, detection_result,
+                               class_names, _MODEL_SIZE)
+
+                    cv2.imshow(win_name, frame)
+
+                    key = cv2.waitKey(1) & 0xFF
+
+                    if key == ord('q'):
+                        break
+
+                    out.write(frame)
+            finally:
+                cv2.destroyAllWindows()
+                cap.release()
+                print('Detections have been saved successfully.')
+
     else:
         raise ValueError("Inappropriate data type. Please choose either 'video' or 'images'.")
 
